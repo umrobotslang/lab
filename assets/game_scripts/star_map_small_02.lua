409fa7190327732e263b69337790738e7f651b41
Vikas Dhiman <dhiman@umich.edu>
2017-04-18 17:56:05 -0400

    Updated the small map 02 with lemon and apple.

--- assets/game_scripts/star_map_small_02.lua
+++ assets/game_scripts/star_map_small_02.lua
@@ -8,25 +8,25 @@ function api:start(episode, seed)
   make_map.seedRng(seed)
   api._count = 0
   api.mapName = "star_map_small_02"
-  -- -- The map has been created and dropped into the assets/maps/ folder
-  --   local entityLevel = [[
-  --  ******
-  --  * A A*
-  --  *  * *    
-  --  ****A*
-  -- ***** *    
-  -- * G**A* 
-  -- * *** *****
-  -- *A A A A  *
-  -- ***** *** *
-  --     *A**P *
-  --     * *****
-  --     *A****   
-  --     * * G*
-  --     *A  A*
-  --     ******
-  --   ]]
-  --   api.mapName = make_map.makeMap(api.mapName, entityLevel)
+  -- The map has been created and dropped into the assets/maps/ folder
+  --  local entityLevel = [[
+  -- ******
+  -- *   A*
+  -- * A* *    
+  -- **** *
+  --***** *    
+  --* G** * 
+  --* *** *****
+  --*A   A   A*
+  --***** *** *
+  --    * **P *
+  --    * *****
+  --    * ****   
+  --    * * L*
+  --    *A   *
+  --    ******
+  --]]
+  --  api.mapName = make_map.makeMap(api.mapName, entityLevel)
 end
 
 function api:commandLine(oldCommandLine)
