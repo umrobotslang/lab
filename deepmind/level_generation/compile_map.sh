698df2fcc712dd93a4e3acb27ae528bba5b92585
Vikas Dhiman <dhiman@umich.edu>
2017-09-14 11:21:02 -0400

    PK3BASE should be readonly variable.

--- deepmind/level_generation/compile_map.sh
+++ deepmind/level_generation/compile_map.sh
@@ -40,7 +40,7 @@
 set -e
 
 readonly MAPBASE="${1}"
-readonly PK3BASE="${2}"
+PK3BASE="${2}"
 
 readonly BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly Q3MP="${BASE}/../../q3map2/q3map2"
